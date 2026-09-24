6425ffbd5f2a0546f4af84673484bbbd4f341daf
deepika-boddu <b.deepu6515@gmail.com>
2026-09-24 13:24:59 +0530

    Update server.py

--- server.py
+++ server.py
@@ -535,9 +535,9 @@ def delete_employee(id):
 # =========================================================
 # START SERVER
 # =========================================================
+create_database()
 
 if __name__ == "__main__":
 
-    create_database()
 
     app.run(debug=True)
